140da1dd5e76dbdbec16e0612c1d52ee1c097b82
arodriguez <angelarodriguezportela@gmail.com>
2024-07-09 19:12:47 +0200

    feat: add constants class and change db password

---
+++ src/main/java/com/anrdez/taskservice/util/Constants.java
@@ -0,0 +1,7 @@
+package com.anrdez.taskservice.util;
+
+public class Constants {
+    public static final String TASK_NOT_FOUND = "Task with id %s not found";
+    public static final String DTO_NOTNULL = "TaskDto must not be null";
+    public static final String ENTITY_NOTNULL = "Task entity must not be null";
+}
